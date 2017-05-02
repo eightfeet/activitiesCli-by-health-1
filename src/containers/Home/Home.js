bee2b40a5ff6fca70e7fe4ee905eb1004dfd9c1b
eightfeet <bajiao000000@163.com>
2017-05-02 23:31:27 +0800

    fix

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 import Modal from '~/components/Modal';
 import Loading from '~/components/Loading';
 import s from './style';
+import prd from './ex.jpg';
 import sl from './styleb';
 
 import face from './face.jpg';
@@ -77,39 +78,74 @@ export default class Home extends Component {
 	render() {
 		console.log(this.context);
 		return (
-			<div>
-				<div className={s.orbg} />
-				<div className={classNames(s.wrap, 'w9', 'center', 'pr')}>
-					<div className={s.card}>
-						<h3 className="w9 center pdt5">李大厨</h3>
-						<ul className={classNames('clearfix', 'nls', 'w9', 'center', s.info)}>
-							<li className="w2 al-r">电话：</li>
-							<li className="w5 font-biggest">13783821431</li>
-							<li className="w3 al-r"><span className={s.btnrz}>去认证</span></li>
-							<li className="w2 al-r">公司：</li>
-							<li className="w8">未知</li>
-							<li className="w2 al-r">邮箱：</li>
-							<li className="w6">暂无</li>
-						</ul>
-						<div className="w9-8 center clearfix">
-							<ul className={classNames(s.banner, 'nls', 'clearfix')}>
-								<li className="w3-3-3 txt_cut">
-									<span className="icon-hot font-biggest"></span>
-									人气:1311123
-								</li>
-								<li className={classNames('w3-3-3', 'txt_cut', s.bdl)}>
-									<span className="icon-zan font-biggest"></span>
-									点赞:1311123
-								</li>
-								<li className={classNames('w3-3-3', 'txt_cut', s.bdl)}>
-									<span className="icon-fav font-biggest"></span>
-									收藏:1311123
-								</li>
+			<div className={s.root}>
+				<div className={classNames(s.wrap, 'center', 'pr')}>
+					<div className={s.orbg} />
+					<div className="w9 center pdb5-5">
+						<div className={s.card}>
+							<h3 className="w9 center pdt5">李大厨</h3>
+							<ul className={classNames('clearfix', 'nls', 'w9', 'center', s.info)}>
+								<li className="w2 al-r">电话：</li>
+								<li className="w5 font-biggest">13783821431</li>
+								<li className="w3 al-r"><span className={s.btnrz}>去认证</span></li>
+								<li className="w2 al-r">公司：</li>
+								<li className="w8">未知</li>
+								<li className="w2 al-r">邮箱：</li>
+								<li className="w6">暂无</li>
 							</ul>
+							<div className="w9-8 center clearfix">
+								<ul className={classNames(s.banner, 'nls', 'clearfix')}>
+									<li className="w3-3-3 txt_cut">
+										<span className="icon-hot font-biggest"></span>
+										人气:1311123
+									</li>
+									<li className={classNames('w3-3-3', 'txt_cut', s.bdl)}>
+										<span className="icon-zan font-biggest"></span>
+										点赞:1311123
+									</li>
+									<li className={classNames('w3-3-3', 'txt_cut', s.bdl)}>
+										<span className="icon-fav font-biggest"></span>
+										收藏:1311123
+									</li>
+								</ul>
+							</div>
 						</div>
-					</div>
-					<div className={s.face}>
-						<img src={face} alt=""/>
+						<div className={s.face}>
+							<img src={face} alt=""/>
+						</div>
+						<div className={s.tabinfo}>
+							<div className="clearfix">
+								<div className={classNames(s.item, s.active)}>作品</div>
+								<div className={s.item}>资料</div>
+							</div>
+							<div className={s.list}>
+								<div className={classNames("clearfix", s.prditem)}>
+									<div className={s.l}>
+										<img src={prd} alt=""/>
+									</div>
+									<div className={s.r}>
+										<h3>手撕鸡</h3>
+										<p>选用三黄鸡为原料，其中，外皮金黄 可谓手撕鸡的卖点之一...</p>
+									</div>
+								</div>
+								<div className={classNames("clearfix", s.prditem)}>
+									<div className={s.l}>
+										<img src={prd} alt=""/>
+									</div>
+									<div className={s.r}>
+										<h3>手撕鸡</h3>
+										<p>选用三黄鸡为原料，其中，外皮金黄 可谓手撕鸡的卖点之一...</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className={s.xsrc}>
+							<h3>相似人才</h3>
+							<div>123</div>
+						</div>
+
+						<div className="btn mgt-8">递交名片</div>
 					</div>
 				</div>
 				<Modal
